92326b8351d052630db7f34574d4c86bf5853797
Steven Haryanto <stevenharyanto@gmail.com>
2012-09-14 06:40:29 +0000

    Import of SHARYANTO/DefHash-1.0.0 from CPAN.
    gitpan-cpan-distribution: DefHash
    gitpan-cpan-version:      1.0.0
    gitpan-cpan-path:         SHARYANTO/DefHash-1.0.0.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/DefHash.pm
@@ -0,0 +1,31 @@
+package DefHash;
+
+our $VERSION = '1.0.0'; # VERSION
+
+1;
+# ABSTRACT: Define things according to a specification, using hashes
+
+__END__
+=pod
+
+=head1 NAME
+
+DefHash - Define things according to a specification, using hashes
+
+=head1 VERSION
+
+version 1.0.0
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
